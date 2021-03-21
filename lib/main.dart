63cc80d5e9eaa10c216246a8caf0561fb7335d34
Gleb Svechnikov <gleb.svechnikov@simplinic.com>
2021-03-21 23:01:28 +0300

    Add fake list of transactions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,15 @@
+import './transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
+
+String getAmountWithCurrency(double amount) {
+  Intl.defaultLocale = 'ru_RU';
+  var format = NumberFormat.simpleCurrency();
+  return '${amount.toString()}  ${format.currencySymbol}';
+}
+
+var uuid = Uuid();
 
 void main() {
   runApp(MyApp());
@@ -16,6 +27,12 @@ class MyApp extends StatelessWidget {
 }
 
 class ExpenseApp extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(id: uuid.v4(), title: 'Electric planer', amount: 3899.00, date: DateTime.utc(2021, 03, 19)),
+    Transaction(id: uuid.v4(), title: 'Groceries', amount: 2822.18, date: DateTime.utc(2021, 03, 20)),
+    Transaction(id: uuid.v4(), title: 'Meds', amount: 222.55, date: DateTime.utc(2021, 03, 21))
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,7 +40,7 @@ class ExpenseApp extends StatelessWidget {
           title: Text('Expenses app'),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Container(
@@ -34,9 +51,40 @@ class ExpenseApp extends StatelessWidget {
                       'CHART',
                       style: TextStyle(color: Colors.black),
                     ))),
-            Card(
-              child: Text('LIST'),
-              elevation: 5,
+            Column(
+              children: transactions
+                  .map(
+                    (transaction) => Card(
+                      child: Row(
+                        children: [
+                          Container(
+                            width: 150,
+                            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                            decoration: BoxDecoration(border: Border.all(color: Colors.grey, width: 2)),
+                            child: Text(
+                              getAmountWithCurrency(transaction.amount),
+                              style: TextStyle(color: Colors.blueGrey, fontSize: 14, fontWeight: FontWeight.bold),
+                            ),
+                            padding: EdgeInsets.all(10),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                transaction.title,
+                                style: TextStyle(color: Colors.blue, fontSize: 16, fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                transaction.date.toString().split(' ')[0], // temp fix due to intl incompatibility
+                                style: TextStyle(color: Colors.grey),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                  .toList(),
             )
           ],
         ));
